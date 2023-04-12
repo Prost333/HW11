336c284cba40487e9d9bc22f21ac3117942886bf
prost333 <dimabolshoy333@yandex.by>
2023-04-12 13:11:16 +0300

    ty

--- src/task2/Department.java
+++ src/task2/Department.java
@@ -4,71 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Department {
-    private String nameDepartment;
-    private List<Room> spisokRoom = new ArrayList<>();
 
-    public Department(String nameDepartment) {
-        this.nameDepartment = nameDepartment;
-    }
-
-    public String getNameDepartment() {
-        return nameDepartment;
-    }
-
-    public void setNameDepartment(String nameDepartment) {
-        this.nameDepartment = nameDepartment;
-    }
-
-    public List<Room> getSpisokRoom() {
-        return spisokRoom;
-    }
-
-    public void setSpisokRoom(List<Room> spisokRoom) {
-        this.spisokRoom = spisokRoom;
-    }
-
-    public void addRoom(Room room) {
-        spisokRoom.add(room);
-    }
-
-
-    //Task2.6
-    public void maleOrFemale() {
-        int sumf = 0;
-        int sumM = 0;
-        for (Room room : spisokRoom) {
-            for (Patient p : room.getPatients()) {
-                if (p.getType() == Types.FEMALE) {
-                    sumf++;
-                } else if (p.getType() == Types.MALE) {
-                    sumM++;
-                }
-            }
-
-        }
-        System.out.println("Female: " + sumf);
-        System.out.println("Male: " + sumM);
-    }
-
-    public void addPatientForDiagnoz(Patient patient) {
-        Room room = new Room(spisokRoom.size()+1 , patient.getType(), "Диарея");
-        for (Room r : spisokRoom) {
-            if (r.getRoomForDiagnos().equals(patient.getDiagnosis())) {
-                r.addPatient(patient);
-                spisokRoom.add(r);
-                System.out.println(patient.getName()+" заселился в палату: ");
-                break;
-
-            } else {
-                Room room1 = new Room(spisokRoom.size()+1 , patient.getType(), patient.getDiagnosis());
-                room1.addPatient(patient);
-                spisokRoom.add(room1);
-                break;
-
-            }
-        }
-
-    }
 }
 
 
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -2,35 +2,7 @@ package task2;
 
 public class Main {
     public static void main(String[] args) {
-        Patient patient1= new Patient(1," Игорь"," Инов","ОРВИ", Types.MALE);
-        Patient patient2= new Patient(2," Саня"," Блатной","Чесотка", Types.MALE);
-        Patient patient3= new Patient(3," Надя"," Хой","ОРВИ", Types.FEMALE);
-        Patient patient4= new Patient(4," Алина"," Волкова","Диарея", Types.FEMALE);
-        Patient patient5= new Patient(5," Юра"," Кот","ОРВИ", Types.MALE);
-        Patient patient6= new Patient(6," Оля"," Хакс","Диарея", Types.FEMALE);
-        Patient patient7= new Patient(7," Юля"," Хакс","Диарея", Types.FEMALE);
-        Patient patient8= new Patient(8," Koma"," Paul","Диарея", Types.FEMALE);
 
-        Room one = new Room(1, Types.FEMALEROOM,"Диарея");
-        one.addPatient(patient3);
-        one.addPatient(patient6);
-        one.addPatient(patient7);
-        Room two = new Room(2, Types.MALE,"ОРВИ");
-        two.addPatient(patient1);
-        two.addPatient(patient2);
-        two.addPatient(patient5);
-        Room three =new Room(3, Types.FEMALEROOM,"Чесотка");
-        three.addPatient(patient4);
-//        two.sortID();
-//        three.sortID();
-        Department head =new Department("Главное управление");
-        head.addRoom(one);
-        head.addRoom(two);
-        head.addRoom(three);
-        head.maleOrFemale();
-        head.addPatientForDiagnoz(patient5);
-        head.addPatientForDiagnoz(patient8);
-        System.out.println(head.getSpisokRoom().size());
 
 
 
